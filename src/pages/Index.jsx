f73e32c7630e97d3d599e38825ac32c27882eb6f
ginnarodriguez <grodriguez47@gmail.com>
2021-12-21 07:14:55 -0500

    front

---
+++ src/pages/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <div className='flex w-full items-center justify-center'>
+      <h1 className='text-2xl font-bold text-gray-900'>
+        Bienvenidos al Proyecto de Ginna Rodriguez
+      </h1>
+    </div>
+  </div>
+);
+
+export default Index;
